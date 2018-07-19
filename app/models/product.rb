bf2ede947dc02c0bae3a84c09fbe6d28066d3066
Bernie Strong <bstrong71@gmail.com>
2018-07-19 17:27:43 -0400

    adds root to store index and basic store page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,11 @@
 class Product < ApplicationRecord
 	validates :title, :description, :image_url, presence: true
 	validates :title, uniqueness: true
+	validates :title, length: { maximum: 30, message: 'must be 30 characters or less' }
+	validates :description, length: { maximum: 300, message: 'must be 300 characters or less' }
 	validates :image_url, allow_blank: true, format: {
 		with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
 	}
-	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+	validates :price, numericality: { greater_than_or_equal_to: 0.01, message: "can't be less than 0.01" }
 end
