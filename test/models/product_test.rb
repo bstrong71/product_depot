aa004c20e42a41b14482806a91c67e0e3e2a5d1d
Bernie Strong <bstrong71@gmail.com>
2018-07-19 15:47:29 -0400

    adds more unit tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+	fixtures :products
   test "product attributes must not be empty" do
   	product = Product.new
   	assert product.invalid?
@@ -29,4 +30,43 @@ class ProductTest < ActiveSupport::TestCase
   	product.price = 1
   	assert product.valid?
   end
+
+  def new_product(image_url)
+  	Product.new(
+  		title: "1 hat",
+  		description: "this is a hat",
+  		price: 1,
+  		image_url: image_url)
+  end
+
+  test "image url" do
+  	ok = %w{ foo.gif foo.jpg foo.png FOO.JPG FOO.Jpg 
+  					http://a.b.c/x/y/z/foo.gif }
+  	bad = %w{ foo.doc foo.gif/more foo.gif.more }
+
+  	ok.each do |image_url|
+  		assert new_product(image_url).valid?,
+  			"#{image_url} shouldn't be invalid" 
+  	end 
+
+  	bad.each do |image_url|
+  		assert new_product(image_url).invalid?,
+  			"#{image_url} shouldn't be valid"
+  	end
+  end
+
+  test "product is not valid without a unique title" do
+  	product = Product.new(
+  		title: products(:three).title,
+  		description: "yyy",
+  		price: 1,
+  		image_url: "foo.gif")
+  	assert product.invalid?
+  	assert_equal ["has already been taken"], 
+  		product.errors[:title]
+  end
 end
+
+
+
+
